654daad18f0a099d616f1736d05691cb2f375fff
Stelkizi <stelkiziridou@gmail.com>
2021-05-14 21:17:16 +0200

    filled Options and Notifications components

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button } from '@material-ui/core';
+import { SocketContext } from '../SocketLogic';
 
 const Notifications = () => {
-  return <div>Notifications</div>;
+  const { call, isCallAccepted, answerCall } = useContext(SocketContext);
+
+  return (
+    <>
+      {call.isCallReceived && !isCallAccepted && (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <h1>{call.name} is calling</h1>
+          <Button variant='contained' color='primary' onClick={answerCall}>
+            Answer
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Notifications;
--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,12 +1,129 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import {
+  Container,
+  TextField,
+  Typography,
+  Grid,
+  Paper,
+  Button,
+  makeStyles,
+} from '@material-ui/core';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Phone, PhoneDisabled, Assignment } from '@material-ui/icons';
 import Notifications from './Notifications';
+import { SocketContext } from '../SocketLogic';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  gridContainer: {
+    width: '100%',
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+  container: {
+    width: '600px',
+    margin: '35px 0',
+    padding: 0,
+    [theme.breakpoints.down('xs')]: {
+      width: '80%',
+    },
+  },
+  margin: {
+    marginTop: 20,
+  },
+  padding: {
+    padding: 20,
+  },
+  paper: {
+    padding: '10px 20px',
+    border: '2px solid black',
+  },
+}));
 
 const Options = () => {
+  const {
+    myDevice,
+    isCallAccepted,
+    isCallEnded,
+    name,
+    setName,
+    terminateCall,
+    callUser,
+  } = useContext(SocketContext);
+  const [idToCall, setIdToCall] = useState('');
+  const classes = useStyles();
+
   return (
-    <div>
-      Options
-      <Notifications />
-    </div>
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete='off'>
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography variant='h6' gutterBottom>
+                Account Info
+              </Typography>
+              <TextField
+                label='Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                fullWidth
+              />
+              <CopyToClipboard text={myDevice} className={classes.margin}>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  fullWidth
+                  startIcon={<Assignment fontSize='large' />}
+                >
+                  Copy your ID
+                </Button>
+              </CopyToClipboard>
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography variant='h6' gutterBottom>
+                Make a call
+              </Typography>
+              <TextField
+                label='Id to call'
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                fullWidth
+              />
+              <CopyToClipboard text={myDevice} className={classes.margin}>
+                {isCallAccepted && !isCallEnded ? (
+                  <Button
+                    variant='contained'
+                    color='secondary'
+                    startIcon={<PhoneDisabled fontSize='large' />}
+                    fullWidth
+                    onClick={terminateCall}
+                    className={classes.margin}
+                  >
+                    Hang Up
+                  </Button>
+                ) : (
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    startIcon={<Phone fontSize='large' />}
+                    fullWidth
+                    onClick={(idToCall) => callUser(idToCall)}
+                    className={classes.margin}
+                  >
+                    Start Call
+                  </Button>
+                )}
+              </CopyToClipboard>
+            </Grid>
+          </Grid>
+        </form>
+        <Notifications />
+      </Paper>
+    </Container>
   );
 };
 
